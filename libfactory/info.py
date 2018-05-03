b6c0379a05f552ed9888204a9f733e727cd6041c
jhover <jhover@starkhorn.rhic.bnl.gov>
2018-05-03 15:54:32 -0400

    Update.

--- libfactory/info.py
+++ libfactory/info.py
@@ -295,3 +295,41 @@ class ObjectIsNotMutable(Exception):
     def __str__(self):
         return repr(self.value)
 
+class GroupByKey(AnalyzerGroup):
+
+    def __init__(self, key):
+        self.key = key
+
+    def group(self, job):
+        try:
+            return job[self.key]
+        except Exception, ex:
+            return None
+
+
+class GroupByKeyRemap(AnalyzerGroup):
+
+    def __init__(self, key, mapping_d):
+        self.key = key
+        self.mapping_d = mapping_d
+
+    def group(self, job):
+        try:
+            value = job[self.key]
+        except Exception, ex:
+            return None
+
+        if value in self.mapping_d.keys():
+            return self.mapping_d[value]
+        else:
+            return None
+
+
+class Length(AnalyzerReduce):
+
+    def __init__(self):
+        pass
+
+    def reduce(self, data):
+        return len(data)
+
